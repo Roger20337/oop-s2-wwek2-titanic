f6f3d22a8e2dedb94a7efb2766db6a11040defe0
Roger Batista <20337@student.dorset-college.ie>
2021-01-26 15:26:41 +0000

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,53 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        // write your code here
+        //   final String DB_URL = "jdbc:mysql://localhost:3306/sakila";
+        final String DB_URL = "jdbc:mysql://localhost:3306/titanicmanifest";
+        final String DB_USER = "Roger";
+        final String DB_PASSWORD = "Todosapopula#15";
 
-        try {
+        try  {
             Class.forName("com.mysql.jdbc.Driver");
 
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/titanicmanifest", "Roger", "Todosapopula#15");
+                    DB_URL, DB_USER, DB_PASSWORD);
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from passengers");
+            String sqlQuery = "select * from titanic";
 
-            System.out.println("before while");
+            ResultSet rs = stmt.executeQuery(sqlQuery);
+
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            for(int i = 1; i<= numberOfColumns; i++) {
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            }
+            System.out.println();
+
 
             while (rs.next()) {
-                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " +
-                        rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5)
-                        + " " + rs.getString(6) + " " + rs.getString(7) + " " + rs.getString(8)
-                        + " " + rs.getString(9) + " " + rs.getString(10) + " " + rs.getString(11));
+                // System.out.println(rs.getInt(1) + "\t  " + rs.getString(2) + " \t " + rs.getString(3));
+                System.out.println(rs.getInt(1) + "\t  " + rs.getInt(2) + " \t " + rs.getInt(3) + " \t " + rs.getString(4));
             }
-            System.out.println("after while");
 
-            con.close();
 
-        } catch (Exception e) {
+
+
+        } catch (SQLException  e) {
             System.out.println(e.toString());
 
-        } finally {
-            System.out.println("finally");
+        } catch(Exception e) {
 
-
+        } finally{
+            // System.out.println("finally");
         }
+
+
     }
 }
-
